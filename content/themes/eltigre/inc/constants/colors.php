fc0ea5d77fb9ef475f6e72527c0ff00fcab0904f
Jullian Leleux <jullian@eltigre.fr>
2021-01-20 16:25:11 +0100

    Colors constants

---
+++ content/themes/eltigre/inc/constants/colors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Eltigre;
+
+define( 'COLORS', array(
+    'FCD125' => array(
+        'name' => 'Yellow',
+        'classname' => 'yellow'
+    ),
+    'FFFFFF' => array(
+        'name' => 'White',
+        'classname' => 'white'
+    ),
+    '000000' => array(
+        'name' => 'Black',
+        'classname' => 'black'
+    )
+) );
